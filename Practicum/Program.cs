8d3f390d980f8cd54b495fed310e14be6bed387f
Beibarsov <BarsStudio@gmail.com>
2022-04-06 10:57:44 +0800

    Update Program.cs

--- Practicum/Program.cs
+++ Practicum/Program.cs
@@ -1,13 +1,14 @@
 ﻿//  создаём пустой список с типом данных Contact
-var phoneBook = new List<Contact>();
+var phoneBook = new List<Contact>()
+{
+new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+};
 
-// добавляем контакты
-phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
-phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
-phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
-phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
-phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
-phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
 
 
 var phoneBookSorted = phoneBook.OrderBy(x => x.Name).ThenBy(x => x.LastName);
